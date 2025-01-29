b84ed633d6414b6c3d3b7c17aaad5ae8eb826b19
Thomas Kruithof <tkruithof95@gmail.com>
2025-01-29 18:59:36 +0100

    test protected routes, middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,6 +15,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
+    // "/profile",
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
