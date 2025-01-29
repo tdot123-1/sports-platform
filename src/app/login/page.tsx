aafefc3bcf8d991b8914e1efc1bab1373ca53f85
Thomas Kruithof <tkruithof95@gmail.com>
2025-01-29 11:39:15 +0100

    add auth middleware

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <>
+      <h1>Login</h1>
+    </>
+  );
+};
+
+export default Page;
---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { updateSession } from "@/lib/supabase/middleware";
+import { type NextRequest } from "next/server";
+
+// calls 'updateSession' for every request to check authentication
+export async function middleware(request: NextRequest) {
+    // either allows access or redirects
+  return await updateSession(request);
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
