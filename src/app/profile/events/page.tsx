780980436c88d8fdda856937296ba1071491f37e
Thomas Kruithof <tkruithof95@gmail.com>
2025-02-05 19:32:01 +0100

    add toolbar to profile/events page

--- src/app/profile/events/page.tsx
+++ src/app/profile/events/page.tsx
@@ -1,8 +1,11 @@
 import EventsList from "@/components/events/events-list";
 import PaginationWrapper from "@/components/events/pagination-wrapper";
 import EventsListSkeleton from "@/components/skeletons/events-list-skeleton";
+import Toolbar from "@/components/toolbar/toolbar";
 import { Skeleton } from "@/components/ui/skeleton";
 import { createClient } from "@/lib/supabase/server";
+import { FilterOptions, SortOptions } from "@/lib/types";
+import { parseFilters, parseSortOptions } from "@/lib/utils";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
@@ -20,19 +23,45 @@ const Page = async (props: {
   }
 
   const searchParams = await props.searchParams;
-  const query = searchParams?.query || "";
+  const query = searchParams?.query
+    ? decodeURIComponent(searchParams.query)
+    : "";
   const currentPage = Number(searchParams?.page) || 1;
+
+  // parse filters
+  let filter: FilterOptions | undefined;
+  if (searchParams) {
+    filter = parseFilters(searchParams);
+  }
+
+  // parse sort options
+  let sort: SortOptions | undefined;
+  if (searchParams) {
+    sort = parseSortOptions(searchParams);
+  }
 
   return (
     <>
-      <h1>My Events</h1>
-      <Suspense fallback={<EventsListSkeleton />}>
-        <EventsList userId={data.user.id} currentPage={currentPage} />
-      </Suspense>
-      <div className="w-fit mx-auto py-6">
-        <Suspense fallback={<Skeleton className="h-6 w-28" />}>
-          <PaginationWrapper query={query} userId={data.user.id} />
+      <Toolbar filter={filter} sort={sort} />
+      <div className="mx-4">
+        <Suspense fallback={<EventsListSkeleton />}>
+          <EventsList
+            userId={data.user.id}
+            currentPage={currentPage}
+            filter={filter}
+            sort={sort}
+            searchQuery={query}
+          />
         </Suspense>
+        <div className="w-fit mx-auto py-6">
+          <Suspense fallback={<Skeleton className="h-6 w-28" />}>
+            <PaginationWrapper
+              query={query}
+              userId={data.user.id}
+              filter={filter}
+            />
+          </Suspense>
+        </div>
       </div>
     </>
   );
